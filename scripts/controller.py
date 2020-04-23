df8bd0aeada2055c370f89c28c5ad51548b24ae0
Naman Gupta <namanryan@gmail.com>
2020-04-22 21:28:27 -0400

    Updated controller.py
    Added PID controller

--- scripts/controller.py
+++ scripts/controller.py
@@ -6,23 +6,62 @@ from geometry_msgs.msg import Twist
 import rospkg
 
 
-# # function to run turtlebot3
-# def runner(order):
-#
-#     # declaring object for class Twist
-#     msg = Twist()
-#     print(msg)
-#
-#     for i in range(len(order)):
-#         rate = rospy.Rate(2)
-#
-#         msg.linear.x = float(order[i][0])
-#         msg.angular.z = float(order[i][1])
-#
-#         rospy.loginfo(msg)
-#         pub.publish(msg)
-#
-#         rate.sleep()
+# function to get Cross-Track Error
+def getCTE(current_pose):
+
+	# coordinates of start point
+	start_pose = (-4, -4)
+
+	# coordinates of goal point
+	goal_pose = (-3, -3)
+
+	# Kp error
+	cte = (((current_pose[1] - start_pose[1])*(goal_pose[0] - start_pose[0])) - \
+		((current_pose[0] - start_pose[0])*(goal_pose[1] - start_pose[0])))/\
+		(((goal_pose[0] - start_pose[0])**2) + ((goal_pose[1] - start_pose[1])**2))
+
+	return cte
+
+
+# function to get angular velocity
+def control(error, prev_error=0, summ=0):
+	
+	# PID parameters
+	Kp, Kd, Ki = 1, 0, 0
+
+	# angular velocity
+	omega = Kp*error + Kd*(error - prev_error) + Ki*summ
+
+	# for Kd and Ki error
+	summ += error
+	prev_error = error
+
+	return omega, summ, prev_error
+
+
+# function to move turtlebot3
+def runner():
+
+    # declaring object for class Twist
+    msg = Twist()
+    odom = Odometry()
+
+    # get current pose
+    current_position = (odom.pose.pose.position.x, odom.pose.pose.position.y)
+
+    # getting error
+    error = getCTE(current_position)
+
+    # getting angular velocity
+    angular_velocity, Ki_error, Kd_error =  control(error, Ki_error, Kd_error)
+
+    # assign angular velocity
+    msg.angular.z = angular_velocity
+    msg.linear.x = 0.2
+
+    # publish angular velocity
+    rospy.loginfo(msg)
+    pub.publish(msg)
 
 
 if __name__ == '__main__':
@@ -36,15 +75,10 @@ if __name__ == '__main__':
     # initialize subscriber node to get robot's odometry
     rospy.Subscriber('odom', Odometry, queue_size=10)
 
-    # get odom data
-    odom = Odometry()
-
     # frequency rate
-    # rate = rospy.Rate(1)
+    rate = rospy.Rate(1)
 
-    x = odom.pose.pose.position.x
-    y = odom.pose.pose.position.y
+    # call function runner
+    runner()
 
-    print('x: ', x, 'y: ', y)
-
-    # rate.sleep()
+    rate.sleep()
