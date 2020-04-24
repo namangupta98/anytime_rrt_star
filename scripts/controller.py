5a7e83cead621fc51536ecdfc55dc2f5c43d8e88
Naman Gupta <namanryan@gmail.com>
2020-04-24 01:27:25 -0400

    Updated subscriber issue

--- scripts/controller.py
+++ scripts/controller.py
@@ -7,7 +7,10 @@ import rospkg
 
 
 # function to get Cross-Track Error
-def getCTE(current_pose):
+def getCTE():
+
+    global current_pose_, cte_
+    current_pose = current_pose_
 
     # coordinates of start point
     start_pose = (-4, -4)
@@ -16,18 +19,17 @@ def getCTE(current_pose):
     goal_pose = (-3, -3)
 
     # Kp error
-    cte = (((current_pose[1] - start_pose[1])*(goal_pose[0] - start_pose[0])) - \
-        ((current_pose[0] - start_pose[0])*(goal_pose[1] - start_pose[0])))/\
-        (((goal_pose[0] - start_pose[0])**2) + ((goal_pose[1] - start_pose[1])**2))
+    cte = (((current_pose[1] - start_pose[1]) * (goal_pose[0] - start_pose[0])) -
+           ((current_pose[0] - start_pose[0]) * (goal_pose[1] - start_pose[0]))) / \
+          (((goal_pose[0] - start_pose[0]) ** 2) + ((goal_pose[1] - start_pose[1]) ** 2))
 
-    return cte
+    cte_ = cte
 
 
 # function to get angular velocity
 def control(error, paramet, errors):
-
     # angular velocity
-    omega = paramet[0]*error + paramet[1]*(error - errors[0]) + paramet[2]*errors[1]
+    omega = paramet[0] * error + paramet[1] * (error - errors[0]) + paramet[2] * errors[1]
 
     # for Kd and Ki error
     errors[1] += error
@@ -41,21 +43,12 @@ def runner(parameters, errors):
 
     # declaring object for class Twist
     msg = Twist()
-    odom = Odometry()
-
-    # loop rate
-    rate = rospy.Rate(1)
-
-    # get current pose
-    current_position = (odom.pose.pose.position.x, odom.pose.pose.position.y)
-    print('current_position', current_position)
-
-    # getting error
-    error = getCTE(current_position)
-    print('error', error)
 
     # getting angular velocity
     angular_velocity, errors = control(error, parameters, errors)
+
+    # loop rate
+    rate = rospy.Rate(1)
 
     # assign angular velocity
     msg.angular.z = angular_velocity
@@ -68,7 +61,20 @@ def runner(parameters, errors):
     rate.sleep()
 
 
+# function to callback subscriber node
+def callback_odom(odom):
+    global current_pose_
+
+    # current position stored
+    current_pose_ = (odom.pose.pose.position.x, odom.pose.pose.position.y)
+
+    # get error
+    getCTE()
+
+
 if __name__ == '__main__':
+
+    global pub
 
     # initialize node
     rospy.init_node('controller', anonymous=True)
@@ -77,7 +83,7 @@ if __name__ == '__main__':
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 
     # initialize subscriber node to get robot's odometry
-    rospy.Subscriber('/odom', Odometry, queue_size=10)
+    sub = rospy.Subscriber('/odom', Odometry, callback_odom)
 
     # PID parameters
     param = [1, 0, 0]
@@ -86,6 +92,5 @@ if __name__ == '__main__':
     err = [0, 0]
 
     # call function runner
-    runner(param, err)
-
-    rospy.spin()
+    while not rospy.is_shutdown():
+        runner(param, err)
